4380e9e54bc98fb23920ceba112f2c8ce35cae0f
Federico Miras <miras.federico@gmail.com>
2018-05-03 17:30:41 -0300

    Write essential tests

---
+++ test/index.js
@@ -0,0 +1,17 @@
+const test = require('ava')
+const powerSum = require('../src')
+
+test('PowerSum 20 and 25, return 3', t => {
+  const result = powerSum(20, 25)
+  t.is(result, 3)
+})
+
+test('PowerSum 25 and 30, return 2', t => {
+  const result = powerSum(25, 30)
+  t.is(result, 2)
+})
+
+test('PowerSum 20 and 20, return 1', t => {
+  const result = powerSum(20, 20)
+  t.is(result, 1)
+})
