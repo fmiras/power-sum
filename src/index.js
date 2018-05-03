3baa3fae5854a26e22ff8e3acc479f7c2df1dd0d
Federico Miras <miras.federico@gmail.com>
2018-05-03 17:26:45 -0300

    Code solution for powersum problem

---
+++ src/index.js
@@ -0,0 +1,40 @@
+module.exports = (i, r) => {
+  let n = 0
+  for (var x = i; x <= r; x++) {
+    console.log(`Checking matching sums for ${x}`)
+    if (matchPowerNumber(x)) {
+      n++
+    }
+  }
+
+  return n
+}
+
+const matchPowerNumber = n => {
+  // Iterate possible bases
+  for (let base1 = 2; base1 < n/2; base1++) {
+    for (let base2 = 2; base2 < n/2; base2++) {
+      if (base1 === base2) {
+        break
+      }
+      if (matchWithBases(base1, base2, n)) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+const matchWithBases = (base1, base2, n) => {
+  // Iterate possible exponents
+  for (let exponent1 = 2; exponent1 < 5; exponent1++) {
+    for (let exponent2 = 2; exponent2 < 5; exponent2++) {
+      let sum = Math.pow(base1, exponent1) + Math.pow(base2, exponent2)
+      if (n === sum) {
+        console.log(`${base1}^${exponent1} + ${base2}^${exponent2} = ${Math.pow(base1, exponent1)} + ${Math.pow(base2, exponent2)} = ${n}`)
+        return true
+      }
+    }
+  }
+  return false
+}
