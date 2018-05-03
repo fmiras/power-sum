7fc74f6c39f87e06e7d55093f07b7dc75bd23a3f
Federico Miras <miras.federico@gmail.com>
2018-05-03 17:35:27 -0300

    XO linting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-module.exports = (i, r) => {
-  let n = 0
-  for (var x = i; x <= r; x++) {
-    console.log(`Checking matching sums for ${x}`)
-    if (matchPowerNumber(x)) {
-      n++
+const matchWithBases = (base1, base2, n) => {
+  // Iterate possible exponents
+  for (let exponent1 = 2; exponent1 < 5; exponent1++) {
+    for (let exponent2 = 2; exponent2 < 5; exponent2++) {
+      const sum = Math.pow(base1, exponent1) + Math.pow(base2, exponent2)
+      if (n === sum) {
+        console.log(`${base1}^${exponent1} + ${base2}^${exponent2} = ${Math.pow(base1, exponent1)} + ${Math.pow(base2, exponent2)} = ${n}`)
+        return true
+      }
     }
   }
-
-  return n
+  return false
 }
 
 const matchPowerNumber = n => {
@@ -25,16 +27,14 @@ const matchPowerNumber = n => {
   return false
 }
 
-const matchWithBases = (base1, base2, n) => {
-  // Iterate possible exponents
-  for (let exponent1 = 2; exponent1 < 5; exponent1++) {
-    for (let exponent2 = 2; exponent2 < 5; exponent2++) {
-      let sum = Math.pow(base1, exponent1) + Math.pow(base2, exponent2)
-      if (n === sum) {
-        console.log(`${base1}^${exponent1} + ${base2}^${exponent2} = ${Math.pow(base1, exponent1)} + ${Math.pow(base2, exponent2)} = ${n}`)
-        return true
-      }
+module.exports = (i, r) => {
+  let n = 0
+  for (let x = i; x <= r; x++) {
+    console.log(`Checking matching sums for ${x}`)
+    if (matchPowerNumber(x)) {
+      n++
     }
   }
-  return false
+
+  return n
 }
